07cb0b6d76cd1b8abcc314b2468e24eb9de544ed
PedroEM <ezemartinex@gmail.com>
2021-09-02 22:33:26 -0300

    arreglo template settings

--- Proyectoinfo/settings.py
+++ Proyectoinfo/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Proyectoinfo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Proyectoinfo/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
